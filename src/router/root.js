7a83d735da7efd7d4e83b8002dbd74d53015e4c6
whguddn <hataraci9350@daum.net>
2025-08-29 10:03:39 +0900

    init
    - 기존 프로젝트 가져왔음

---
+++ src/router/root.js
@@ -0,0 +1,23 @@
+import { ClipLoader } from "react-spinners";
+import ListPage from "../pages/phonecase/ListPage";
+import { createBrowserRouter } from "react-router-dom";
+import DetailPage from "../pages/phonecase/DetailPage";
+
+import CartPage from "../pages/phonecase/CartPage";
+
+const root = createBrowserRouter([
+  {
+    path: "phonecase",
+    element: <ListPage></ListPage>,
+  },
+  {
+    path: "detail",
+    element: <DetailPage></DetailPage>,
+  },
+  {
+    path: "cart",
+    element: <CartPage></CartPage>,
+  },
+]);
+
+export default root;
